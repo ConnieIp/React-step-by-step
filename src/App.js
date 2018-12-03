fb2f9d3bca842f7aae4d49d78bd9ee96030dfa85
Connie Ip <connie.ip@cargosmart.com>
2018-12-03 14:43:52 +0800

    Hello World button alert

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    alertText(){
+        alert('now you know how to handle onclick event')
+    }
     render() {
-        return ( <div><button>Hello World!</button>
+        return ( <div><button onClick={this.alertText}>Hello World!</button>
         <span>number :0</span></div>
         );
     }
