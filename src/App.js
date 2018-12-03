afba7322c07c305b81a51ee54e678280383710c5
Connie Ip <connie.ip@cargosmart.com>
2018-12-03 15:44:59 +0800

    App accept default number

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 class App extends Component {
     state={number:0}
     updateNumber=()=>{
-        this.setState({number:1})
+        // this.setState({number:1})
+        this.setState({number:this.props.defaultNum})
     }
     render() {
         return ( <div><button onClick={this.updateNumber}>Hello World!</button>
